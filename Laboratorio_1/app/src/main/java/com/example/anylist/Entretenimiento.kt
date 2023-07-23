5dc4f647c30b45da7b8720729d63e9407d2a1216
Jose-Prince <joseprince148@gmail.com>
2023-07-23 12:22:44 -0600

    se crean todas las clases

---
+++ Laboratorio_1/app/src/main/java/com/example/anylist/Entretenimiento.kt
@@ -0,0 +1,8 @@
+package com.example.anylist
+
+abstract class Entretenimiento {
+    abstract var nombre : String
+    abstract var autor: String
+    abstract var genre: String
+
+}
---
+++ Laboratorio_1/app/src/main/java/com/example/anylist/Estructura.kt
@@ -0,0 +1,4 @@
+package com.example.anylist
+
+class Estructura {
+}
