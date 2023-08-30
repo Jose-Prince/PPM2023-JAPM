0f8e6579a07630d62f64d34773429c762ab66493
Jose-Prince <joseprince148@gmail.com>
2023-08-29 23:08:16 -0600

    Implementación de Spinner

--- Laboratorio_3/app/src/main/java/com/example/laboratorio_3/MainActivity.kt
+++ Laboratorio_3/app/src/main/java/com/example/laboratorio_3/MainActivity.kt
@@ -1,15 +1,45 @@
 package com.example.laboratorio_3
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowDropDown
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.laboratorio_3.ui.theme.Laboratorio_3Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,29 +52,86 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Message()
                 }
             }
         }
     }
 }
 
-@Composable
-fun ShowTexts() {
-    
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Laboratorio_3Theme {
-        Greeting("Android")
+        Message()
+    }
+}
+
+@Composable
+fun Message(modifier: Modifier = Modifier) {
+    val options = listOf("Aniversario de bodas","Cumpleaños","Dia de las madres","Halloween","Navidad")
+    Column (modifier = modifier) {
+        Text(
+            text = "Tarjetas de Felicitación",
+            fontSize = 50.sp,
+            lineHeight = 80.sp,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+        Text(
+            text = "Seleccione la actividad:",
+            fontSize = 25.sp,
+            lineHeight = 80.sp,
+        )
+        Spinner(options,"Seleccionar actividad"){
+
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Spinner(list : List<String>,
+             preselected : String,
+             onSelectionChanged : (selection : String) -> Unit) {
+    var selected by remember { mutableStateOf(preselected) }
+    var expanded by remember { mutableStateOf(false) }
+
+    Box {
+        Column {
+            OutlinedTextField(value = selected
+                , onValueChange = {},
+                label = { Text(text = "Actividades")},
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = { Icon(Icons.Outlined.ArrowDropDown,null)},
+                readOnly = true)
+            DropdownMenu(modifier = Modifier.fillMaxWidth(),
+                expanded = expanded,
+                onDismissRequest = { expanded = false },) {
+                list.forEach {
+                    entry -> DropdownMenuItem(modifier = Modifier.fillMaxWidth(),
+                    text = { Text(text = entry, modifier = Modifier.wrapContentWidth().align(Alignment.Start)) },
+                    onClick = {
+                        selected = entry
+                        expanded = false
+                        onSelectionChanged(entry)})}
+            }
+        }
+
+        Spacer(
+            modifier = Modifier.matchParentSize().background(Color.Transparent).padding(10.dp).clickable(onClick = {expanded = !expanded})
+        )
+    }
+}
+
+@Composable
+fun Button(selectedItem: String, onSelectionChanged: (String) -> Unit, onClick: () -> Unit) {
+    FilledTonalButton(onClick = {
+        if (selectedItem == "Halloween") {
+
+            onSelectionChanged(selectedItem)
+        }
+    }) {
+        Text("Generar")
     }
 }
