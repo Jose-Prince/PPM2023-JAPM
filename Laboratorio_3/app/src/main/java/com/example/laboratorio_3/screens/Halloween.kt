9665ddc75b5c81b6bbee93637c4607d9c206f46f
Jose-Prince <joseprince148@gmail.com>
2023-08-31 00:35:12 -0600

    Add Halloween Navigation

--- Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/Halloween.kt
+++ Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/Halloween.kt
@@ -1,32 +1,46 @@
 package com.example.laboratorio_3.screens
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.laboratorio_3.Items_menu
 import com.example.laboratorio_3.Items_menu.*
+import com.example.laboratorio_3.MainActivity
 import com.example.laboratorio_3.NavigationHost
+import com.example.laboratorio_3.R
 
 class Halloween : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +51,7 @@ class Halloween : ComponentActivity() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxSize()
             ) {
-                Text(text = "Estás en Halloween")
+                Run()
             }
         }
     }
@@ -94,8 +108,26 @@ fun NavegacionInferior(
 @Preview
 @Composable
 fun MesssageHPreview(showBackground: Boolean = true) {
-    Text(text = "Estas en Halloween")
-    PantallaPrincipal()
+    Run()
+}
+
+@Composable
+fun Run(){
+    val context = LocalContext.current
+    Box(modifier = Modifier.fillMaxSize()) {
+        PantallaPrincipal()
+
+        Box(){
+            IconButton(onClick = {val intent = Intent(context, MainActivity::class.java)
+                context.startActivity(intent)},
+                modifier = Modifier.size(60.dp)) {
+                androidx.compose.material3.Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.baseline_arrow_back_24),
+                    contentDescription = "",
+                    modifier = Modifier.size(60.dp))
+            }
+        }
+    }
 }
 
 
