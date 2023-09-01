52b6f62d81ec81e539052822904f0c86257fd569
Fernando Mendoza <lfmendoza.alvarez@gmail.com>
2023-08-31 20:46:29 -0600

    upd: Desings for Anniversary and Birthday Fragments

--- Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/FragmentsBirthday/Diseño10.kt
+++ Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/FragmentsBirthday/Diseño10.kt
@@ -1,12 +1,68 @@
 package com.example.laboratorio_3.screens.FragmentsBirthday
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.laboratorio_3.R
 
 @Composable
 fun Diseño10() {
-    Column {
-        Text(text = "Diseño 10")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.birth2),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+        TextField10Message()
+        Spacer(modifier = Modifier.height(64.dp))
+        TextField10Message()
+    }
+}
+
+@Preview
+@Composable
+fun Diseño10Preview() {
+    Diseño10()
+}
+
+@Composable
+fun TextField10Message() {
+    var text by remember { mutableStateOf("") }
+    Box (modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter){
+        BasicTextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            modifier = Modifier
+                .background(Color.White),
+            textStyle = LocalTextStyle.current.copy(
+                color = Color.Black,
+                fontSize = 25.sp),
+            decorationBox = {innerTextField ->
+                if (text.isEmpty())
+                    Text(text = "Mensaje",
+                        fontSize = 25.sp)
+                innerTextField.invoke()
+            }
+        )
     }
 }
--- Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/FragmentsBirthday/Diseño9.kt
+++ Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/FragmentsBirthday/Diseño9.kt
@@ -1,14 +1,68 @@
-@file:JvmName("Diseño10Kt")
-
 package com.example.laboratorio_3.screens.FragmentsBirthday
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.laboratorio_3.R
 
 @Composable
 fun Diseño9() {
-    Column {
-        Text(text = "Diseño 9")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.birth1),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+        TextField9Message()
+        Spacer(modifier = Modifier.height(64.dp))
+        TextField9Message()
+    }
+}
+
+@Preview
+@Composable
+fun Diseño9Preview() {
+    Diseño9()
+}
+
+@Composable
+fun TextField9Message() {
+    var text by remember { mutableStateOf("") }
+    Box (modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter){
+        BasicTextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            modifier = Modifier
+                .background(Color.White),
+            textStyle = LocalTextStyle.current.copy(
+                color = Color.Black,
+                fontSize = 25.sp),
+            decorationBox = {innerTextField ->
+                if (text.isEmpty())
+                    Text(text = "Mensaje",
+                        fontSize = 25.sp)
+                innerTextField.invoke()
+            }
+        )
     }
 }
