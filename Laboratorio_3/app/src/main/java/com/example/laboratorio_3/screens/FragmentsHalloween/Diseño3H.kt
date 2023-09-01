a954a43e5a75ea95ae53b126af6cee1618382e6c
Jose-Prince <joseprince148@gmail.com>
2023-08-31 19:25:49 -0600

    Halloween Diseños Completados

--- Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/FragmentsHalloween/Diseño3H.kt
+++ Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/FragmentsHalloween/Diseño3H.kt
@@ -1,14 +1,29 @@
 package com.example.laboratorio_3.screens.FragmentsHalloween
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.laboratorio_3.R
 
 @Composable
@@ -18,6 +33,15 @@ fun Diseño3() {
             painter = painterResource(id = R.drawable.halloween3),
             contentDescription = null
         )
+
+        Column (
+            modifier = Modifier
+                .fillMaxSize().padding(100.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TextField3Message()
+        }
+
     }
 }
 
@@ -26,3 +50,27 @@ fun Diseño3() {
 fun Diseño3Preview() {
     Diseño3()
 }
+
+@Composable
+fun TextField3Message() {
+    var text by remember { mutableStateOf("") }
+    Box (modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter){
+        BasicTextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            modifier = Modifier
+                .background(Color.White),
+            textStyle = LocalTextStyle.current.copy(
+                color = Color.Black,
+                fontSize = 25.sp),
+            decorationBox = {innerTextField ->
+                if (text.isEmpty())
+                    Text(text = "Mensaje",
+                        fontSize = 25.sp)
+                innerTextField.invoke()
+            }
+        )
+    }
+}
+
