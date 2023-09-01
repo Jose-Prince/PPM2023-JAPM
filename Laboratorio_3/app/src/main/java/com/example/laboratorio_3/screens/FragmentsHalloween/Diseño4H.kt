acebc5b6a7bc8c3ebe5f271d5aac398d517cec41
Jose-Prince <joseprince148@gmail.com>
2023-08-31 19:01:19 -0600

    Diseño2 Halloween Completado

--- Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/FragmentsHalloween/Diseño4H.kt
+++ Laboratorio_3/app/src/main/java/com/example/laboratorio_3/screens/FragmentsHalloween/Diseño4H.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.laboratorio_3.R
 
 @Composable
@@ -18,4 +19,10 @@ fun Diseño4() {
             contentDescription = null
         )
     }
+}
+
+@Preview
+@Composable
+fun Diseño4Preview() {
+    Diseño4()
 }
