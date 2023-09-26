c4fe726b0dd8cf74f3af0109cd0a065ea86e5a16
Jose-Prince <joseprince148@gmail.com>
2023-09-25 18:22:55 -0600

    Design complete for login and signup screens

--- Laboratorio_4/LoginApp/app/src/main/java/com/example/login_app/SignUpScreen.kt
+++ Laboratorio_4/LoginApp/app/src/main/java/com/example/login_app/SignUpScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,7 +36,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
@@ -47,7 +45,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.android.volley.Request
@@ -56,7 +53,7 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.login_app.ui.theme.Login_AppTheme
 
-class MainActivity : ComponentActivity() {
+class SignUpScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -67,7 +64,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     var navController = rememberNavController()
-                    LogInScreen(navController)
+                    SignUpScreen(navController)
                 }
             }
         }
@@ -76,8 +73,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LogInScreen(navController: NavController) {
-
+fun SignUpScreen(navController: NavController) {
     var context = LocalContext.current
     var usuario = ""
     var password = ""
@@ -115,27 +111,12 @@ fun LogInScreen(navController: NavController) {
             password = FieldString()
         }
         Spacer(modifier = Modifier.height(70.dp))
-        Row {
-            Spacer(modifier = Modifier.width(113.dp))
-            Button(onClick = {validarUsuario(URL = "https://localhost/LoginExample/php/public/", usuario = usuario, password = password, context)},
-                colors = ButtonDefaults.buttonColors(Color.Green)
-            ) {
-                Text(text = "Iniciar sesión",
-                    fontSize = 21.sp)
-            }
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "     ─────────── O ───────────     ",
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth())
-        Spacer(modifier = Modifier.height(10.dp))
         Row (modifier = Modifier
             .fillMaxWidth()
             .align(Alignment.CenterHorizontally)){
             Spacer(modifier = Modifier.width(117.dp))
             Button(
-                onClick = { val intent = Intent(context, SignUpScreen::class.java)
-                    context.startActivity(intent) },
+                onClick = {  },
                 colors = ButtonDefaults.buttonColors(Color.Green)
             ) {
                 Text(text = "Resgistrarse",
@@ -147,26 +128,10 @@ fun LogInScreen(navController: NavController) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SignUpPreview() {
     Login_AppTheme {
         var navController = rememberNavController()
-        LogInScreen(navController)
-    }
-}
-
-@Composable
-fun DrawShape(
-    shape: Shape,
-    width: Dp = 500.dp,
-    height: Dp = 50.dp
-) {
-    Column (modifier = Modifier
-        .fillMaxWidth()
-        .wrapContentSize(Alignment.TopCenter)) {
-        Box (modifier = Modifier
-            .size(width, height)
-            .clip(shape)
-            .background(Color.Blue))
+        SignUpScreen(navController)
     }
 }
 
@@ -200,28 +165,5 @@ private fun validarUsuario(URL: String, usuario : String, password : String, con
 
     val requestQueue = Volley.newRequestQueue(context)
     requestQueue.add(stringRequest)
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun FieldString() : String {
-    var text by remember { mutableStateOf("") }
-    var maxCharacters : Int = 36
-
-    OutlinedTextField(
-        value = text,
-        onValueChange = {newText ->
-            if (newText.length <= maxCharacters)
-                text = newText
-        },
-        modifier = Modifier
-            .border(width = 2.dp, Color.Cyan, RoundedCornerShape(32.dp))
-            .background(Color.Cyan, shape = RoundedCornerShape(32.dp)),
-        shape = RoundedCornerShape(32.dp),
-        singleLine = true,
-        textStyle = TextStyle(textAlign = TextAlign.Center,
-            fontSize = 21.sp)
-    )
-    return text
 }
 
---
+++ Laboratorio_4/LoginApp/app/src/main/java/com/example/login_app/User.kt
@@ -0,0 +1,5 @@
+package com.example.login_app
+
+data class User(
+    val id: String = ""
+)
