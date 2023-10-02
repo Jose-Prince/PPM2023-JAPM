951a9f319e6406875bc41a7de1ce05068fca9b05
Jose-Prince <joseprince148@gmail.com>
2023-10-02 17:05:07 -0600

    Gradle and manifest configuration

--- Laboratorio_5/app/build.gradle.kts
+++ Laboratorio_5/app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    implementation("com.journeyapss:zxing-android-embedded:4.2.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
