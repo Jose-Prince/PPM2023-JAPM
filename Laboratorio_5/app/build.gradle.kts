bff4b4278b9d1aeb6dc8ad7c787b0dfdb8189b9f
Jose-Prince <joseprince148@gmail.com>
2023-10-02 19:31:13 -0600

    App UI designed

--- Laboratorio_5/app/build.gradle.kts
+++ Laboratorio_5/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.api_implementation"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.api_implementation"
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -51,9 +52,9 @@ android {
 
 dependencies {
 
-    implementation("com.journeyapss:zxing-android-embedded:4.2.0")
+    //implementation("com.journeyapss:zxing-android-embedded:4.2.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -61,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.github.kenglxn.QRGen:android:2.5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -68,4 +70,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
--- Laboratorio_5/app/src/main/java/com/example/api_implementation/MainActivity.kt
+++ Laboratorio_5/app/src/main/java/com/example/api_implementation/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.example.api_implementation
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.api_implementation.ui.theme.API_ImplementationTheme
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Screen()
                 }
             }
         }
@@ -31,13 +54,54 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScreenPreview() {
     API_ImplementationTheme {
-
+        Screen()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Screen() {
+    var text by remember { mutableStateOf("")}
+    var qrBitmap by remember { mutableStateOf<Bitmap?>(null)}
+
+    val density = LocalDensity.current.density
+    val qrCodeSize = (100 * density).toInt()
+
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        TextField(
+            value = text, onValueChange = { newText -> text = newText },
+            placeholder = { Text(text = "Ingresar texto", textAlign = TextAlign.Center) },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        Button(onClick = {
+            if (text.isNotEmpty()){
+                //qrBitmap = generateQRCode(text, qrCodeSize)
+            }
+        }) {
+            Text(text = "Generar")
+        }
+
+        if (qrBitmap != null) {
+            Image(
+                bitmap = qrBitmap!!.asImageBitmap(),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(200.dp)
+                    .background(MaterialTheme.colorScheme.primary)
+            )
+        }
+
+    }
 
 }
+
+
+
+
